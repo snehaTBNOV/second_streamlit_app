03a13e1cd3b8eafe008e6dc5e23b684aa775c3f8
snehaTBNOV <144178731+snehaTBNOV@users.noreply.github.com>
2024-01-29 15:45:44 +0000

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,7 @@ import streamlit
 import snowflake.connector
 
 streamlit.title('hii');
- my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
 CURRENT_REGION()")
